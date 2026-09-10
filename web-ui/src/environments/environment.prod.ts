dd2b998b07b5f5518b4e0a529b5f6da8d9c9ce00
Bemi Irone <bemidev@gmail.com>
2026-09-10 15:39:46 +0100

    fix: change env prod url to secure url

--- web-ui/src/environments/environment.prod.ts
+++ web-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://ec2-18-209-159-172.compute-1.amazonaws.com:3000',
+  apiBaseUrl: 'https://apiagencyos.nsmultimedia.co.uk',
 };
