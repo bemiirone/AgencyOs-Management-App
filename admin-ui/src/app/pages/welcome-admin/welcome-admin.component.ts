95a2ad13cadd5401426143f175bc4a66736cc95a
Bemi Irone <bemidev@gmail.com>
2026-09-21 11:47:25 +0100

    fix: linting error

--- admin-ui/src/app/pages/welcome-admin/welcome-admin.component.ts
+++ admin-ui/src/app/pages/welcome-admin/welcome-admin.component.ts
@@ -95,6 +95,7 @@ export class WelcomeAdminComponent implements OnInit {
           }
         }
       },
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       error: () => {},
     });
   }
